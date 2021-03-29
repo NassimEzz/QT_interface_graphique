dfe3549624dd544ffc36b70b6c84b4bdb7eaaca1
ezzayani <ezzayani@ensicaen.ecole.fr>
2021-03-29 10:27:22 +0200

    button open and quit

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFileDialog>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -8,9 +9,17 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
     connect(ui->messageText,SIGNAL(textChanged(QString)),ui->binWidget,SLOT(newMessage(QString)));
     connect(ui->messageText,SIGNAL(textChanged(QString)),ui->paraWidget,SLOT(newMessage(QString)));
+    connect(ui->actionQuit,SIGNAL(triggered(bool)),this,SLOT(close()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+
+void MainWindow::on_actionOpen_triggered()
+{
+    QString file = QFileDialog::getOpenFileName(this,"Choose a file","/home/eleves/promo22/info/ezzayani","Text files (*.txt)");
 }
