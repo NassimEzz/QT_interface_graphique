1cdbd84c00053270bb81d321e081e922a66d49a5
Majd Butty <majd.butty@ecole.ensicaen.fr>
2021-03-29 21:26:28 +0200

    Added translation file to project repository

---
+++ para_fr.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="unfinished">Encodage de Parachute</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="26"/>
+        <source>Message:  </source>
+        <translation type="unfinished">Message</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="55"/>
+        <source>&amp;File</source>
+        <translation type="unfinished">&amp;Fichier</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="64"/>
+        <source>&amp;About</source>
+        <translation type="unfinished">&amp;A Propos</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="81"/>
+        <source>&amp;Open</source>
+        <translation type="unfinished">&amp;Ouvrir</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="86"/>
+        <source>&amp;Save</source>
+        <translation type="unfinished">&amp;Sauvegarder</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="91"/>
+        <source>&amp;Quit</source>
+        <translation type="unfinished">&amp;Quitter</translation>
+    </message>
+</context>
+</TS>
