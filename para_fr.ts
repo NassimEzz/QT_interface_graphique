7064ca600859f88d5ba5b4bcfe461a96dd667722
JumpOff <hackingontop@gmail.com>
2021-03-30 08:36:16 +0200

    Updated translation

--- para_fr.ts
+++ para_fr.ts
@@ -9,32 +9,42 @@
         <translation>Encodage de Parachute</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="26"/>
+        <location filename="mainwindow.ui" line="23"/>
         <source>Message:  </source>
         <translation>Message</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="55"/>
+        <location filename="mainwindow.ui" line="30"/>
+        <source>Number of Sectors: </source>
+        <translation>Nombre de Secteurs</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="37"/>
+        <source>Number of Tracks:</source>
+        <translation>Nombre de Pistes</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="89"/>
         <source>&amp;File</source>
         <translation>&amp;Fichier</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="64"/>
+        <location filename="mainwindow.ui" line="98"/>
         <source>&amp;About</source>
         <translation>&amp;A Propos</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="81"/>
+        <location filename="mainwindow.ui" line="115"/>
         <source>&amp;Open</source>
         <translation>&amp;Ouvrir</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="86"/>
+        <location filename="mainwindow.ui" line="120"/>
         <source>&amp;Save</source>
         <translation>&amp;Sauvegarder</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="91"/>
+        <location filename="mainwindow.ui" line="125"/>
         <source>&amp;Quit</source>
         <translation>&amp;Quitter</translation>
     </message>
