a4fe39d1da3bd37a94c792e338ac5cf2b9ff3b9e
Majd Butty <majd.butty@ecole.ensicaen.fr>
2021-04-05 19:32:33 +0200

    Added translation depending on system language

--- main.cpp
+++ main.cpp
@@ -5,10 +5,16 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    QString qmFile = QString(":/para_fr.qm");
-    QTranslator * translator = new QTranslator(&a);
-    translator->load(qmFile);
-    a.installTranslator(translator);
+    QStringList languages = QLocale().uiLanguages();
+    if (languages.size()) {
+        QString language = languages.front().split("-").front();
+        QString qmFile = QString(":/para_%1.qm").arg(language);
+        if (QFileInfo(qmFile).exists()) {
+            QTranslator * translator = new QTranslator(&a);
+            translator->load(qmFile);
+            a.installTranslator(translator);
+        }
+    }
 
     MainWindow w;
     w.show();
